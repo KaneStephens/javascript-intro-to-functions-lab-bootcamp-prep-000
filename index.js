468ca62c4be17d9809ff7494d72dd22c9d14d1f0
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-07-07 11:40:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var tooQuiet = string
-  if (tooQuiet.toLowerCase === tooQuiet) {
+  if (string.toLowerCase === string) {
     return "I can't hear you!"
   }
 }
